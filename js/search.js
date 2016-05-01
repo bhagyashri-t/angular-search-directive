361c0c992dd5bc5cd5f00bcfd6487e57447a90de
unknown <bhagyashri.tamkhane@gmail.com>
2016-05-01 16:24:05 -0700

    Commented code and removed console logs

--- js/search.js
+++ js/search.js
@@ -1,24 +1,22 @@
 
-
 var searchDirectiveApp = angular.module('searchDirectiveApp', ['ngRoute']);
-
 
 searchDirectiveApp.controller('searchController', [ '$scope', 'actorsFactory', 'moviesFactory', 'directorsFactory', function($scope, actorsFactory, moviesFactory, directorsFactory) {
 	$scope.actors = actorsFactory.actors;
 	$scope.movies = moviesFactory.movies;
 	$scope.directors = directorsFactory.directors;
-	console.log("Directors: " + $scope.directors + "Actors: " + $scope.actors);
+
 	$scope.searchData = [$scope.actors, $scope.movies, $scope.directors];
 }]);
 
 searchDirectiveApp.controller('actorsController', [ '$scope', 'actorsFactory', function($scope, actorsFactory) {
 	$scope.actors = actorsFactory.actors;
-	console.log("Actors: " + $scope.actors);
+
 	$scope.searchData = [$scope.actors];
 }]);
 
 searchDirectiveApp.controller('moviesController', [ '$scope', 'moviesFactory', function($scope, moviesFactory) {
 	$scope.movies = moviesFactory.movies;
-	console.log("Movies: " + $scope.movies);
+
 	$scope.searchData = [$scope.movies];
 }]);
